8e7d20c0cf524842cfc4b2f85cc7e6e474ccc497
NanaManuel007 <nuel.application@gmail.com>
2025-09-18 07:14:55 +0000

    Fix AllUsersMain component data population and API response handling

--- src/lib/services/api.ts
+++ src/lib/services/api.ts
@@ -3,8 +3,8 @@
 export const API_CONFIG = {
     // BASE_URL: 'https://node.jobtiondevs.com/api/jobtion/', 
     IMAGE_URL: 'https://minio-kogckgccwg40sgkksc4k4o4w.46.202.141.196.sslip.io/jobtion/', 
-    // BASE_URL: 'http://localhost:8080/api/', 
-    BASE_URL: 'https://productionapttmh.quinteduhosting.com/api/',  
+    BASE_URL: 'http://localhost:8080/api/', 
+    // BASE_URL: 'https://productionapttmh.quinteduhosting.com/api/',  
     ENDPOINTS: {
         AUTH: {
             LOGIN: 'admin/auth/login',
